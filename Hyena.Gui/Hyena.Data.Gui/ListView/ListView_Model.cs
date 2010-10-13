cdf210fd71de69003c356c56c2259d23501c3226
Gabriel Burt <gabriel.burt@gmail.com>
2010-10-13 15:11:34 -0500

    [ListView] Avoid NRE if ColumnController is null

--- Hyena.Gui/Hyena.Data.Gui/ListView/ListView_Model.cs
+++ Hyena.Gui/Hyena.Data.Gui/ListView/ListView_Model.cs
@@ -66,7 +66,7 @@ namespace Hyena.Data.Gui
             }
 
             ISortable sortable = model as ISortable;
-            if (sortable != null) {
+            if (sortable != null && ColumnController != null) {
                 ISortableColumn sort_column = ColumnController.SortColumn ?? ColumnController.DefaultSortColumn;
                 if (sort_column != null) {
                     sortable.Sort (sort_column);
